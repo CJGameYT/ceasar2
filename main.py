9681b9989933dd0e8866a302687aaa33bdfc045c
CJGameYT <cjgamesyt111@gmail.com>
2024-09-27 11:12:56 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+secret_code = "this is secret code"
+
+vowel = "a", "e", "i", "o", "u", "y"
+upper_vowel = "A", "E", "I", "O", "U", "Y"
+
+
+def cipher(text: str, shift: int):
+    result = ""
+
+    for char in secret_code():
+        if char == vowel():
+            pass
+
+
+    
+
+print(vowel)
